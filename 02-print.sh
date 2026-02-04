f5547e0f2ce3bcc09e3a2e65d09d6562f2a9e005
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-04 23:45:45 +0530

    modified

--- 02-print.sh
+++ 02-print.sh
@@ -7,9 +7,9 @@ echo hello world
 # \e[0m - to disable color which is enabled
 # COL stands for color and possible colors are RED(31), GREEN(32), YELLOW(33), BLUE(34), MAGENTA(35), CYAN(36)
 
-echo -e "\e[31m Hello in Red Color\e[0m"
-echo -e "\e[32m Hello in Green Color\e[0m"
-echo -e "\e[33m Hello in Yellow Color\e[0m"
-echo -e "\e[34m Hello in Blue Color\e[0m"
-echo -e "\e[35m Hello in Magenta Color\e[0m"
-echo -e "\e[36m Hello in Cyan Color\e[0m"
+echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Magenta Color\e[0m"
+echo -e "\e[36mHello in Cyan Color\e[0m"

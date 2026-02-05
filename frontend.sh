5f2c81f9517d6e3cdde7bc31232a0254b77c6928
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-05 23:44:50 +0530

    redirectors

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,18 @@
 echo -e "\e[33mInstalling Nginx Server\e[0m"
-dnf install nginx -y 
+dnf install nginx -y > /tmp/roboshop.log
 
-echo -e "\e[33mRemoving the old app content\e[0m"
-rm -rf /usr/share/nginx/html/* 
+echo -e "\e[33mRemoving Old App content\e[0m"
+rm -rf /usr/share/nginx/html/* > /tmp/roboshop.log
 
-echo -e "\e[33mdownloading the app code\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+echo -e "\e[33mdownloading Frontend Content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip > /tmp/roboshop.log
 
-echo -e "\e[33mExtracting the app code\e[0m"
+echo -e "\e[33mExtracting Frontend Content\e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip > /tmp/roboshop.log
 
 # vim /etc/nginx/default.d/roboshop.conf 
 
-echo -e "\e[33mstarting the nginx server\e[0m"
+echo -e "\e[33mStarting Nginx Server\e[0m"
 systemctl enable nginx
 systemctl restart nginx

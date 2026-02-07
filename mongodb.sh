f38533210ba2bdee87969d25025ea7ccec454788
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-07 14:19:40 +0530

    mongod

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,14 @@
 echo -e "\e[33mSetuping the MongoDB repo file\e[0m"
-cp  mongo.repo /etc/yum.repos.d/
+cp  mongo.repo /etc/yum.repos.d/ &>> /tmp/roboshop.log
 
 echo -e "\e[33mInstalling MongoDB\e[0m"
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>> /tmp/roboshop.log
 
 echo -e "\e[33mEnabling MongoDB\e[0m"
-systemctl enable mongod 
+systemctl enable mongod &>> /tmp/roboshop.log
 
 echo -e "\e[33mUpdating listen address\e[0m"
-sed -i "/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+sed -i  "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf 
 
 echo -e "\e[33mRestarting MongoDB\e[0m"
-systemctl restart mongod
+systemctl restart mongod &>> /tmp/roboshop.log

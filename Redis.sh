e6da54f56366505791936747d259b4b9e6c7bc6d
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-09 18:03:13 +0530

    redis

---
+++ Redis.sh
@@ -0,0 +1,21 @@
+#setting up the hostname
+echo -e "\e[33msetting the hostname\e[0m"
+hostnamectl set-hostname redis
+
+#Enable Redis 6.2 from package streams.
+echo -e "\e[33menable the redis 6.2 package\e[0m"
+dnf module disable redis -y &>> /tmp/roboshop.log
+dnf module enable redis:6 -y &>> /tmp/roboshop.log
+
+#Install Redis
+echo -e "\e[33minstall the redis\e[0m"
+dnf install redis -y &>> /tmp/roboshop.log
+
+#Edit the config file
+echo -e "\e[33m editing the config file\e[0m"
+sed -i "s/127.0.0.1/0.0.0.0" /etc/redis/redis.conf
+
+#start and enable the redis service
+echo -e "\e[33m enable and restart service\e[0m"
+systemctl enable redis &>> /tmp/roboshop.log 
+systemctl restart redis &>> /tmp/roboshop.log

8e90500582eb9087fd7688347d27e439a21eb9d2
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-17 17:44:20 +0530

    loops

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,7 +1,7 @@
 #if we want to repeat the same code multiple times, we will use loops concept
 
-a = 10
-while [ $a -gt 0] ; do
+a=10
+while [ $a -gt  0 ]; do
     echo hello world
     a = $(($a - 1))
 done
